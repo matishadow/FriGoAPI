4f706659c16f17f336703fbcbc79c4a7dee18a2c
matishadow <matishadow@gmail.com>
2017-03-12 11:57:47 +0100

    return type to Json

--- FriGo/FriGo.Api/App_Start/WebApiConfig.cs
+++ FriGo/FriGo.Api/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using FluentValidation.WebApi;
 using FriGo.Api.Filters;
@@ -20,6 +21,9 @@ namespace FriGo.Api
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             config.Filters.Add(new ValidateModelStateFilter());
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
