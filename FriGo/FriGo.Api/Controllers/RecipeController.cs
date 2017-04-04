c23bed8bd512db7d64ae5061fe95d31a6efef6d6
Unknown <mar.maleszewski@gmail.com>
2017-04-04 12:40:51 +0200

    add logic in get methods in RecipeController

--- FriGo/FriGo.Api/Controllers/RecipeController.cs
+++ FriGo/FriGo.Api/Controllers/RecipeController.cs
@@ -9,13 +9,16 @@ using FriGo.Db.Models;
 using FriGo.Db.Models.Recipes;
 using FriGo.ServiceInterfaces;
 using Swashbuckle.Swagger.Annotations;
+using System.Linq;
 
 namespace FriGo.Api.Controllers
 {
     public class RecipeController : BaseFriGoController
     {
-        public RecipeController(IMapper autoMapper) : base(autoMapper)
+        private readonly IRecipeService recipeService;
+        public RecipeController(IMapper autoMapper, IRecipeService recipeService) : base(autoMapper)
         {
+            this.recipeService = recipeService;
         }
 
         /// <summary>
@@ -26,7 +29,11 @@ namespace FriGo.Api.Controllers
         [SwaggerResponse(HttpStatusCode.OK, Type = typeof(RecipeDto))]
         public virtual HttpResponseMessage Get(Guid id)
         {
-            throw new NotImplementedException();
+            Recipe recipeResult = recipeService.Get().FirstOrDefault(x => x.Id == id);
+            if (recipeResult != null)
+                return Request.CreateResponse(HttpStatusCode.OK, recipeResult);
+            else
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"Recipe {id} don not exist");
         }
 
         /// <summary>
@@ -40,12 +47,21 @@ namespace FriGo.Api.Controllers
         /// <param name="tagQuery">Search by tags</param>
         /// <returns></returns>
         [SwaggerResponse(HttpStatusCode.OK, Type = typeof(RecipeDto))]
-        public virtual HttpResponseMessage Get(int page = 0, int perPage = 10, string sortField = null,
+        public virtual HttpResponseMessage Get(int page = 1, int perPage = 10, string sortField = null,
             bool descending = false, string nameSearchQuery = null, Tag[] tagQuery = null)
         {
-            throw new NotImplementedException();
-        }
+            IEnumerable<Recipe> returnValue = recipeService.Get()
+                                                    .FilterByName(nameSearchQuery)
+                                                    .FilterByTag(tagQuery)
+                                                    .SortByField(sortField, descending)
+                                                    .Skip((page - 1) * perPage).Take(perPage);
 
+            if (returnValue.Count() > 0)
+                return Request.CreateResponse(HttpStatusCode.OK, returnValue);
+            else
+                return Request.CreateResponse(HttpStatusCode.NoContent);
+        }
+        
         /// <summary>
         /// Create new recipe
         /// </summary>
@@ -73,6 +89,57 @@ namespace FriGo.Api.Controllers
         public virtual HttpResponseMessage Delete(Guid id)
         {
             throw new NotImplementedException();
+        }
+    }
+
+    internal static class EngineRecipeSearch
+    {
+        public static IEnumerable<Recipe> FilterByName(this IEnumerable<Recipe> recipes, string nameSearchQuery)
+        {
+            try
+            {
+                return recipes.Where(x => x.Title.Contains(nameSearchQuery));
+            }
+            catch (ArgumentNullException)
+            {
+                return recipes;
+            }
+        }
+        public static IEnumerable<Recipe> FilterByTag(this IEnumerable<Recipe> recipes, Tag[] tags)
+        {
+            try
+            {
+                return recipes.Where(x => x.Tags.Select(y => y.Name)
+                                     .Intersect(tags.Select(z => z.Name))
+                                     .Any());
+            }
+            catch (ArgumentNullException)
+            {
+                return recipes;
+            }   
+        }
+        public static IEnumerable<Recipe> SortByField(this IEnumerable<Recipe> recipes, string field, bool descending)
+        {
+            try
+            {
+                foreach (var Property in typeof(Recipe).GetType().GetProperties())
+                {
+                    if (field.ToLower() == Property.Name.ToLower())
+                    {
+                        Func<Recipe, object> key = x => Property.GetValue(x, null);
+                        return descending ? recipes.OrderByDescending(key) : recipes.OrderBy(key);
+                    }
+                }
+                return recipes.OrderBy(x => x.Rating);
+            }
+            catch (ArgumentNullException)
+            {
+                return new List<Recipe>();
+            }
+            catch (NullReferenceException)
+            {
+                return recipes;
+            }   
         }
     }
 }
