29f0b39f25b818500cd023c894ee3fc24cf21c84
matishadow <matishadow@gmail.com>
2017-05-05 01:12:56 +0200

    Add account validation

--- FriGo/FriGo.Db/Models/Authentication/ChangePasswordBindingModel.cs
+++ FriGo/FriGo.Db/Models/Authentication/ChangePasswordBindingModel.cs
@@ -4,9 +4,12 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation.Attributes;
+using FriGo.Db.ModelValidators;
 
 namespace FriGo.Db.Models.Authentication
 {
+    [Validator(typeof(ChangePasswordValidator))]
     public class ChangePasswordBindingModel
     {
         public string OldPassword { get; set; }
--- FriGo/FriGo.Db/Models/Authentication/RegisterBindingModel.cs
+++ FriGo/FriGo.Db/Models/Authentication/RegisterBindingModel.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation.Attributes;
+using FriGo.Db.ModelValidators;
 
 namespace FriGo.Db.Models.Authentication
 {
+    [Validator(typeof(RegisterValidator))]
     public class RegisterBindingModel
     {
         public string Email { get; set; }
